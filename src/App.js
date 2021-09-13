930d4575eaf5c8e8a50291ea6e5a0b827298dfb3
RajPatel <raj191@live.com>
2021-09-13 10:45:22 -0400

    title change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Maze from "./components/Maze";
 function App() {
   return (
     <div className="App">
-    <h1 >Solve a maze from 0 to 99</h1>
+    <h1 >Create a Path from 0 to 99</h1>
     <Maze />
     </div>
   );
