5d5a182a0ac097f617521579572177eb5787ec23
RajPatel <raj191@live.com>
2021-09-16 11:16:32 -0400

    a few finishing touches

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Heading from "./components/Heading";
 import Maze from "./components/Maze";
 
 function App() {
   return (
     <div className="App">
-    <h1 >Create a Path from 0 to 99</h1>
+    <Heading/>
     <Maze />
+    
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+const Heading = () => {
+    return ( 
+        <div className="heading-wrapper">
+            <h1 className="title">Interactive Maze Solver</h1>
+            <p className="add">Create a path going from 0 to 99</p>
+            <p className="add">Click on a box to make it a wall</p>
+        </div>
+     );
+}
+ 
+export default Heading;
